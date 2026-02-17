6a323386e12c8ea782cb5e67112ff3b127d8a00a
Omkar Kamat <mail4omkarkamat@gmail.com>
2026-02-18 00:08:00 +0530

    configure cors middleware

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 const API_PREFIX = "/api/v1";
 
 app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
