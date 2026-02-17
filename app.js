0c01ffa657016e5264480c6d6835d37790475548
Omkar Kamat <mail4omkarkamat@gmail.com>
2026-02-18 00:06:04 +0530

    feat: add helmet security middleware

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import helmet from "helmet";
 
 const app = express();
-
 const API_PREFIX = "/api/v1";
 
-app.use(express.json());
+app.use(helmet());
+
+app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.get(`${API_PREFIX}/health`, (req, res) => {
