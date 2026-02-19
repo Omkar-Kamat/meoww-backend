b10ea147ce358b180fc7d48dd35aecc29942c636
Omkar Kamat <mail4omkarkamat@gmail.com>
2026-02-19 19:24:02 +0530

    implement secure profile update endpoint with validation and restricted fields

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,19 +1,36 @@
 import UserService from "../services/user.service.js";
 
 class UserController {
-  // get profile
-  static async getProfile(req, res, next) {
-    try {
-      const user = await UserService.getProfile(req.user.id);
+    // get profile
+    static async getProfile(req, res, next) {
+        try {
+            const user = await UserService.getProfile(req.user.id);
 
-      res.status(200).json({
-        status: "success",
-        data: user,
-      });
-    } catch (error) {
-      next(error);
+            res.status(200).json({
+                status: "success",
+                data: user,
+            });
+        } catch (error) {
+            next(error);
+        }
     }
-  }
+
+    // update profile
+    static async updateProfile(req, res, next) {
+        try {
+            const updatedUser = await UserService.updateProfile(
+                req.user.id,
+                req.body,
+            );
+
+            res.status(200).json({
+                status: "success",
+                data: updatedUser,
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
 }
 
 export default UserController;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import UserController from "../controllers/user.controller.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import { updateProfileSchema } from "../validations/user.schema.js";
+
 
 const router = express.Router();
 
 router.get("/profile", authMiddleware, UserController.getProfile);
+
+router.patch("/profile", authMiddleware, validate(updateProfileSchema), UserController.updateProfile);
 
 export default router;
